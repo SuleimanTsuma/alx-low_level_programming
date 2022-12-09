38688aa0f55b670adedf2a8e2ceaa6cbccaa1e05
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-09 11:12:45 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+
+a = 'y';
+while
+(a >= 'b') {
+putchar(a);
+a--;
+}
+putchar('\n');
+return (0);
+}
+
+
