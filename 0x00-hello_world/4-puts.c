ad41497c96c18206da02ed6ac2198fcdc8da1643
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-08 16:43:35 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-	/**
-	 * main - Entry point
-	 *
-	 * Description: 'first program'
-	 *
-	 * Return: Always (0) sucess
-	 */
+/**
+ * main - Entry point
+ *
+ * Description: 'first program'
+ *
+ * Return: Always (0) sucess
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
