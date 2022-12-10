043f6628c1ba04308e4575320d5a46b1eef27fc7
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-09 23:24:58 -0800

    Dorar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <unistdio.h>
+#include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful
+ * main - prints exactly and that piece of art
  *
  * Return: Always 0 (Success)
  */
