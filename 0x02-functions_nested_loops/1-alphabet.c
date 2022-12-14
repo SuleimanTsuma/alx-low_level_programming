a0522c74f8411e0b95661c84d2148f8b28e37876
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-14 08:47:01 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+_putchar(c);
+c++;
+
+}
+
+_putchar('\n');
+
+return (0);
+}
