a638f103cd9ae7240d3da31706fc23dabd8c782f
SuleimanTsuma <117924282+SuleimanTsuma@users.noreply.github.com>
2022-12-15 17:40:04 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - checks english character
- *
- * @c: parameter
- * Return: 1 for english character, 0 for non English character
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-return (1);
-else
-return (0);
-
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
