2c7943e8ff0fdade86d33b04d44d83b6aa6d698b
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-15 22:34:08 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
