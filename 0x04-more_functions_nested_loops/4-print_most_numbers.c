6006af4c7cfe2e11f78cfd2550a7fefec748dbbb
SuleimanTsuma <suleimantsuma123@gmail.com>
2022-12-15 22:32:08 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
-	}
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
