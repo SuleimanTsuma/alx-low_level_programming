0857656a15859ceb9f655f36456acaf3065296e8
SuleimanTsuma <suleimantsuma123@gmail.com>
2023-01-03 02:26:00 -0800

    edited

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char c)
@@ -10,3 +13,4 @@ char *_strstr(char *haystack, char *needle)
 void print_chessboard(char (*a)[8])
 void print_diagsums(int *a, int size)
 void set_string(char **s, char *to)
+#endif
