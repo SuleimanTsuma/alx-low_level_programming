365227f668aea6344d0d2906ab53f075acec7060
SuleimanTsuma <suleimantsuma123@gmail.com>
2023-01-04 08:09:20 -0800

    the game is fine

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -6,7 +6,6 @@
  * @to: string
  * Return: void
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,7 +6,6 @@
  *
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
