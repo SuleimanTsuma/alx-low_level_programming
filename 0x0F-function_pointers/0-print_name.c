634c63acf2229e21fa35385debd23d236bef5133
SuleimanTsuma <suleimantsuma123@gmail.com>
2023-01-19 07:29:27 -0800

    the commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,6 @@
  *
  * Return: no return
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,5 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
+
 #endif /* _FUNCTION_POINTERS_H_ */
