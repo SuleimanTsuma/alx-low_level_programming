343be5eb02b21827f7bc1cb1754d290bd41acb50
SuleimanTsuma <suleimantsuma123@gmail.com>
2023-02-09 12:19:02 -0800

    headers

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -12,6 +12,7 @@
  *
  * Return: 0 if the first n bytes of s1 and s2 are equal, otherwise non-zero
  */
+
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
 	for ( ; n && *s1 && *s2; --n, ++s1, ++s2)
